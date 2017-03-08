488761fedc3c6268ebb225c2a019af6c31dc6b60
NetPatch Lab <netpatch.co@gmail.com>
2017-03-08 11:11:06 +0800

    Integrate amt2 into amt

--- README.md
+++ README.md
@@ -1,30 +1,38 @@
-## make country ips files(country_ips/*.txt)
+## Country ips groups(country_ips/*.txt)
 
+source:
+
+1, ftp://ftp.ripe.net/pub/stats/ripencc/delegated-ripencc-extended-latest
+2, ftp://ftp.lacnic.net/pub/stats/lacnic/delegated-lacnic-extended-latest
+3, ftp://ftp.arin.net/pub/stats/arin/delegated-arin-extended-latest
+4, ftp://ftp.apnic.net/public/stats/apnic/delegated-apnic-extended-latest
+5, ftp://ftp.afrinic.net/pub/stats/afrinic/delegated-afrinic-extended-latest
+
+make youself groups:
 
     python make_country_ips.py
     
-
 <https://firewall.netpatch.co/country_ips/>
 
-## make Ad/Malware/Tracking domains file(amt.txt)
 
+## Ad/Malware/Tracking group(amt.txt)
+
+sources:
+
+1, <https://pgl.yoyo.org/as/>
+2, <https://adblockplus.org/subscriptions>
+
+make yourself group
 
     python make_domain_group.py
     
-
 <https://firewall.netpatch.co/files/amt.txt>
 
-## make domains file from adblock plus's list(amt2.txt)
 
-
-    python make_domain_group_from_adblockplus.py
-    
-
-<https://firewall.netpatch.co/files/amt2.txt>
-
-## Add domains or domain source
+## Add domain names or sources
 
 Please create an issue, thanks.
+
 
 ## Download NetPatch Firewall
 
--- make_domain_group.py
+++ make_domain_group.py
@@ -1,36 +1,88 @@
-import os, urlparse, urllib2, math, urllib
+import os.path, re, urlparse, urllib2
 
 domain_source_urls = [
     ('https://pgl.yoyo.org/as/serverlist.php?hostformat=nohtml&showintro=0', 'root_domain'),
-    ('http://www.abclite.cn/Abclite_ADB.conf', 'surge'),
-    ('https://raw.githubusercontent.com/BeliefanX/surge/master/surge_rules_09.02.conf', 'surge'),
-    ('https://raw.githubusercontent.com/BurpSuite/CloudGate-Surge/master/Surge.Conf', 'surge'),
+
+    ('https://easylist-downloads.adblockplus.org/easylist.txt', 'adblock'),
+    ('https://raw.githubusercontent.com/ABPindo/indonesianadblockrules/master/subscriptions/abpindo.txt', 'adblock'),
+    ('http://stanev.org/abp/adblock_bg.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/easylistchina.txt', 'adblock'),
+    ('https://raw.githubusercontent.com/cjx82630/cjxlist/master/cjx-annoyance.txt', 'adblock'),
+    ('https://raw.github.com/tomasko126/easylistczechandslovak/master/filters.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/easylistdutch.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/easylistgermany.txt', 'adblock'),
+    ('https://raw.githubusercontent.com/easylist/EasyListHebrew/master/EasyListHebrew.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/easylistitaly.txt', 'adblock'),
+    ('http://margevicius.lt/easylistlithuania.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/easylistspanish.txt', 'adblock'),
+    ('https://notabug.org/latvian-list/adblock-latvian/raw/master/lists/latvian-list.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/Liste_AR.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/liste_fr.txt', 'adblock'),
+    ('http://www.zoso.ro/pages/rolist.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/advblock.txt', 'adblock'),
+
+    ('https://easylist-downloads.adblockplus.org/easyprivacy.txt', 'adblock'),
+    ('https://adblock.gardar.net/is.abp.txt', 'adblock'),
+    ('http://www.void.gr/kargig/void-gr-filters.txt', 'adblock'),
+    ('http://bit.ly/11QrCfx', 'adblock'),
+    ('https://raw.githubusercontent.com/zpacman/Blockzilla/master/Blockzilla.txt', 'adblock'),
+    ('http://adblock.dajbych.net/adblock.txt', 'adblock'),
+    ('http://adblock.ee/list.php', 'adblock'),
+    ('http://gurud.ee/ab.txt', 'adblock'),
+    ('http://abp.mozilla-hispano.org/nauscopio/filtros.txt', 'adblock'),
+    ('https://raw.githubusercontent.com/szpeter80/hufilter/master/hufilter.txt', 'adblock'),
+    ('https://adblock.dk/block.csv', 'adblock'),
+    ('http://noads.it/filtri.txt', 'adblock'),
+    ('https://raw.githubusercontent.com/yous/YousList/master/youslist.txt', 'adblock'),
+
+    ('https://easylist-downloads.adblockplus.org/antiadblockfilters.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/adwarefilters.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/fanboy-annoyance.txt', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/fanboy-social.txt', 'adblock'),
+    ('http://www.kiboke-studio.hr/i-dont-care-about-cookies/abp/', 'adblock'),
+    ('https://easylist-downloads.adblockplus.org/malwaredomains_full.txt', 'adblock'),
+    ('https://raw.github.com/liamja/Prebake/master/obtrusive.txt', 'adblock'),
+    ('https://raw.githubusercontent.com/Dawsey21/Lists/master/adblock-list.txt', 'adblock'),
+
 ]
 
 def get_file_name(url):
     return os.path.split(urlparse.urlparse(url).path)[1]
 
-def get_content_from_url(url):
-    file_name = get_file_name(url)
-    if os.path.exists('/tmp/'+file_name) and False:
+def get_content_from_url(url, key):
+    file_name = 'nf_'+get_file_name(url)+key
+    if os.path.exists('/tmp/'+file_name):
         return open('/tmp/'+file_name).read()
     else:
-        content = urllib2.urlopen(url).read()
+        user_agent = 'Wget/1.16'
+        req = urllib2.Request(url, headers={'User-Agent': user_agent})
+        content = urllib2.urlopen(req).read()
         f = open('/tmp/'+file_name, 'w')
         f.write(content)
         f.close()
         return content
 
-whitelist_domains = set(['.gvt1.com'])
+whitelist_domains = set([])
 
 def make_domain_group(urls):
+    pass_re = re.compile('[\$\%\*=|\#&@\/\?\+,;:]')
+    ip_pattern = re.compile(r"^(([01]?\d\d?|2[0-4]\d|25[0-5])\.){3}([01]?\d\d?|2[0-4]\d|25[0-5])$")
+
     res = set()
+    url_index = 0
     for url, type in urls:
-        content = get_content_from_url(url).split('\n')
+        url_index += 1
+        try:
+            content = get_content_from_url(url, str(url_index)).split('\n')
+        except:
+            print 'get_content_from_url error: ', type, url
+            raise
+
         for line in content:
-            i = line.find('#')
-            if i!=-1:
-                line = line[0:i]
+            if type!='adblock':
+                i = line.find('#')
+                if i!=-1:
+                    line = line[0:i]
 
             line = line.strip()
             if not line:
@@ -50,6 +102,21 @@ def make_domain_group(urls):
                 domain = line
                 if '.' in domain:
                     res.add('.'+domain)
+            elif type=='adblock':
+                if line.startswith('||') and not pass_re.search(line[2:]): 
+                    line = line[2:]
+                    if line[-1] in ('.', '-', '_'):
+                        continue
+                    line = line[:-1]
+                    if line[-1] in ('.', '-', '_'):
+                        continue
+
+                    if ip_pattern.match(line):
+                        continue
+
+                    domain = line
+                    if '.' in domain:
+                        res.add('.'+domain)
     
     res = res - whitelist_domains
     res = list(res)
